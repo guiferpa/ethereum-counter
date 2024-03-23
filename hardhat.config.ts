b3cb1e8213378d8da1517b4978882ff4d4b6a55f
Guilherme Paixão <guiferpa@gmail.com>
2024-03-23 02:02:22 -0300

    added config for polygon testnet (Mumbai)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: "https://public.stackup.sh/api/v1/node/polygon-mumbai",
+      accounts: [`${vars.get("WALLET_PRIVATE_KEY")}`],
+    },
+  },
 };
 
 export default config;
